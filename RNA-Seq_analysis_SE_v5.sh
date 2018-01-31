714ed48dcc1a771ff51118f2eeaf9ac02deb6ead
David Manahan <dmanahan@usc.edu>
2018-01-31 10:50:18 -0800

    Update RNA-Seq_analysis_SE_v5.sh

--- RNA-Seq_analysis_SE_v5.sh
+++ RNA-Seq_analysis_SE_v5.sh
@@ -1,45 +1,55 @@
-GEO_list='cat <GEO_list>'
+#Define your varible: Enter the filename used to download your dataset:
+GEO_list='<SRA_list>'
 
-for i in $(${GEO_list}); do
+#Convert .sra to .fastq
+for i in $(cat ${GEO_list}); do
   qsub -v IN=${i} ~/cmb/sra_fastq_conversion_SE.pbs &
 done
 
-for i in $(${GEO_list}); do
+#Pause pipeline until all .fastq are generated
+for i in $(cat ${GEO_list}); do
   until [ -f ${i}.fastq ]; do
     sleep 5
   done
 done
 
-for i in $(${GEO_list}); do
+#Report your read quality
+for i in $(cat ${GEO_list}); do
   qsub -v IN=${i} ~/cmb/fastqc_script_SE.pbs &
 done
 
-for i in $(${GEO_list}); do
+#Pause pipeline until all reads are QC'd
+for i in $(cat ${GEO_list}); do
   until [ -f ${i}_fastqc.html ]; do
     sleep 5
   done
 done
 
-for i in $(${GEO_list}); do
+#Adapter and quality trim your reads
+for i in $(cat ${GEO_list}); do
   qsub -v IN=${i} ~/cmb/trimgalore_SE_script.pbs &
 done
 
-for i in $(${GEO_list}); do
+#Pause pipeline until all reads are trimmed for quality and adapter content
+for i in $(cat ${GEO_list}); do
   until [ -f ${i}_trimmed_fastqc.html ]; do
     sleep 5
   done
 done
 
-for i in $(${GEO_list}); do
+#Align trimmed reads to your reference genome
+for i in $(cat ${GEO_list}); do
   qsub -v IN=${i} ~/cmb/STAR_hg38_SE.pbs &
 done
 
-for i in $(${GEO_list}); do
+#Pause pipeline until all reads are mapped
+for i in $(cat ${GEO_list}); do
   until [ -f ${i}Aligned.sortedByCoord.out.bam ]; do
     sleep 5
   done
 done
 
-for i in $(${GEO_list}); do
+#Convert read mapping data to raw gene count matrices
+for i in $(cat ${GEO_list}); do
   qsub -v IN=${i} ~/cmb/bam_counts.pbs &
 done
