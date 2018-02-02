662d0d2762aa36fdd33ae5da68888f116b583464
David Manahan <dmanahan@usc.edu>
2018-02-02 15:54:39 -0800

    Update downloads.sh

--- downloads.sh
+++ downloads.sh
@@ -1,1 +1,1 @@
-for i in $(cat FILENAME); do x=$(echo ${i} | cut -c1-6); wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/${x}/${i}/$i.sra; done
+for i in $(cat accession_list); do x=$(echo ${i} | cut -c1-6); wget ftp://ftp-trace.ncbi.nlm.nih.gov/sra/sra-instant/reads/ByRun/sra/SRR/${x}/${i}/$i.sra; done
