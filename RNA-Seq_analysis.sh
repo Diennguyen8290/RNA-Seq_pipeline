ae0fcc07c5674b17c15262050f4efe2fc2d847f3
David Manahan <dmanahan@usc.edu>
2018-02-01 17:53:42 -0800

    Update RNA-Seq_analysis.sh

--- RNA-Seq_analysis.sh
+++ RNA-Seq_analysis.sh
@@ -4,7 +4,7 @@ GEO_list='GSE73727_list'
 #Specify whether the library in question is single-end ('SE') or paired-end ('PE').
 library='SE'
 
-#Specify from which organism your data is coming from.  Human='hg38', rat ='Rnor6.0'
+#Specify from which organism your data is coming from.  Human='hg38', rat ='Rnor6.0' 
 organism='hg38'
 
 #CONVERT .SRA TO .FASTQ
@@ -12,20 +12,18 @@ for i in $(cat ${GEO_list}); do
   qsub -v IN=${i} ../fastq-dump_${library}.pbs &
 done
 
-if ${library} eq 'SE'
-  then
-    for i in $(cat ${GEO_list}); do
-      until [ -f ${i}.fastq ]; do
-        sleep 5
-      done
+if [ ${library} = 'SE' ]; then
+  for i in $(cat ${GEO_list}); do
+    until [ -f ${i}.fastq ]; do
+      sleep 5
     done
-elif ${library} eq 'PE'
-  then
-    for i in $(cat ${GEO_list}); do
-      until [ -f ${i}_2.fastq ]; do
-        sleep 5
-      done
+  done
+elif [ ${library} = 'PE' ]; then
+  for i in $(cat ${GEO_list}); do
+    until [ -f ${i}_2.fastq ]; do
+      sleep 5
     done
+  done
 else
   echo 'Please specify whether your library is single-end or paired-end'
   exit
@@ -36,20 +34,18 @@ for i in $(cat ${GEO_list}); do
   qsub -v IN=${i} ../fastqc_${library}.pbs &
 done
 
-if ${library} eq 'SE'
-  then
-    for i in $(cat ${GEO_list}); do
-      until [ -f ${i}_fastqc.html ]; do
-        sleep 5
-      done
+if [ ${library} = 'SE' ]; then
+  for i in $(cat ${GEO_list}); do
+    until [ -f ${i}_fastqc.html ]; do
+      sleep 5
     done
-elif ${library} eq 'PE'
-  then
-    for i in $(cat ${GEO_list}); do
-      until [ -f ${i}_2_val_2_fastqc.html ]; do
-        sleep 5
-      done
+  done
+elif [ ${library} = 'PE' ]; then
+  for i in $(cat ${GEO_list}); do
+    until [ -f ${i}_2_val_2_fastqc.html ]; do
+      sleep 5
     done
+  done
 else
   echo 'Please specify whether your library is single-end or paired-end'
   exit
@@ -60,20 +56,18 @@ for i in $(cat ${GEO_list}); do
   qsub -v IN=${i}  ../trimgalore_${library}.pbs &
 done
 
-if ${library} eq 'SE'
-  then
-    for i in $(cat ${GEO_list}); do
-      until [ -f ${i}_trimmed_fastqc.html ]; do
-        sleep 5
-      done
+if [ ${library} = 'SE' ]; then
+  for i in $(cat ${GEO_list}); do
+    until [ -f ${i}_trimmed_fastqc.html ]; do
+      sleep 5
     done
-elif ${library} eq 'PE'
-  then
-    for i in $(cat ${GEO_list}); do
-#      until [ -f ${i}_2.fastq ]; do
-        sleep 5
-      done
+  done
+elif [ ${library} = 'PE' ]; then
+  for i in $(cat ${GEO_list}); do
+#    until [ -f ${i}_2.fastq ]; do
+      sleep 5
     done
+  done
 else
   echo 'Please specify whether your library is single-end or paired-end'
   exit
