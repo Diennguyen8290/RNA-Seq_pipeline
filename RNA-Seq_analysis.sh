ec25cc1305bfbdee39ac730b70737f95659d0026
David Manahan <dmanahan@usc.edu>
2018-03-06 14:44:25 -0800

    Update RNA-Seq_analysis.sh

--- RNA-Seq_analysis.sh
+++ RNA-Seq_analysis.sh
@@ -3,89 +3,80 @@
 #Specify whether the library in question is single-end ('SE') or paired-end ('PE').
 library='SE or PE'
 
-#Specify from which organism your data is coming from.  Human='hg38', rat ='Rnor6.0' 
-organism='hg38 or Rnor6.0'
+#Specify from which organism your data is coming from.  Human='hg38', rat='Rnor6', mouse='mg38' 
+organism='hg38 or Rnor6 or mg38'
 
 #######################################################################################################
+chmod 775 *sra
 
 #FASTQ-DUMP
-for i in $(cat accession_list); do
-  qsub -v IN=${i} ../fastq-dump_${library}.pbs &
+mkdir FASTQ-DUMP
+cd FASTQ-DUMP
+
+for i in $(cat ../accession_list); do
+  qsub -v IN=${i} ../../pbs_scripts/fastq-dump/fastq-dump_${library}.pbs &
 done
 
-if [ ${library} = 'SE' ]; then
-  for i in $(cat accession_list); do
-    until [ -f ${i}.fastq ]; do
-      sleep 5
-    done
-  done
-elif [ ${library} = 'PE' ]; then
-  for i in $(cat accession_list); do
-    until [ -f ${i}_2.fastq ]; do
-      sleep 5
-    done
-  done
-else
-  echo 'Please specify whether your library is single-end or paired-end'
-  exit
-fi
-
-#FASTQC
-for i in $(cat accession_list); do
-  qsub -v IN=${i} ../fastqc_${library}.pbs &
-done
-
-if [ ${library} = 'SE' ]; then
-  for i in $(cat accession_list); do
-    until [ -f ${i}_fastqc.html ]; do
-      sleep 5
-    done
-  done
-elif [ ${library} = 'PE' ]; then
-  for i in $(cat accession_list); do
-    until [ -f ${i}_2_val_2_fastqc.html ]; do
-      sleep 5
-    done
-  done
-else
-  echo 'Please specify whether your library is single-end or paired-end'
-  exit
-fi
-
-#TRIMGALORE!
-for i in $(cat accession_list); do
-  qsub -v IN=${i}  ../trimgalore_${library}.pbs &
-done
-
-if [ ${library} = 'SE' ]; then
-  for i in $(cat accession_list); do
-    until [ -f ${i}_trimmed_fastqc.html ]; do
-      sleep 5
-    done
-  done
-elif [ ${library} = 'PE' ]; then
-  for i in $(cat accession_list); do
-    until [ -f ${i}_2_val_2.fq ]; do
-      sleep 5
-    done
-  done
-else
-  echo 'Please specify whether your library is single-end or paired-end'
-  exit
-fi
-
-#STAR
-for i in $(cat accession_list); do
-  qsub -v IN=${i} ../STAR_${organism}_${library}.pbs &
-done
-
-for i in $(cat accession_list); do
-  until [ -f ${i}Aligned.sortedByCoord.out.bam ]; do
+for i in $(cat ../accession_list); do
+  until [ -f ${i}_fastq-dump_${library}.error ]; do
     sleep 5
   done
 done
 
+cd ..
+
+#FASTQC
+mkdir FASTQC
+cd FASTQC
+
+for i in $(cat ../accession_list); do
+  qsub -v IN=${i} ../../pbs_scripts/FastQC/fastqc_${library}.pbs &
+done
+
+for i in $(cat ../accession_list); do
+  until [ -f ${i}_fastqc_${library}.error ]; do
+    sleep 5
+  done
+done
+
+cd ..
+
+#TRIMGALORE!
+mkdir TRIMGALORE
+cd TRIMGALORE
+
+for i in $(cat ../accession_list); do
+  qsub -v IN=${i}  ../../pbs_scripts/TrimGalore/trimgalore_${library}.pbs &
+done
+
+for i in $(cat ../accession_list); do
+  until [ -f ${i}_TrimGalore_${library}.error ]; do
+    sleep 1m
+  done
+done
+
+cd ..
+
+#STAR
+mkdir STAR
+cd STAR
+
+for i in $(cat ../accession_list); do
+  qsub -v IN=${i} ../../pbs_scripts/STAR/STAR_${organism}_${library}.pbs &
+done
+
+for i in $(cat ../accession_list); do
+  until [ -f ${i}Log.final.out ]; do
+    sleep 1m
+  done
+done
+
+cd ..
+
 #HTSeq
-for i in $(cat accession_list); do
-  qsub -v IN=${i} ../htseq_${organism}.pbs &
+mkdir HTSeq
+cd HTSeq
+
+for i in $(cat ../accession_list); do
+  qsub -v IN=${i} ../../pbs_scripts/HTSeq/htseq_${organism}.pbs &
 done
