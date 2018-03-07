0d69b5aaa434a2017daf5995de7ab933af40d56b
David Manahan <dmanahan@usc.edu>
2018-03-07 14:13:12 -0800

    Update and rename download_refereces.sh to download_references.sh

--- download_refereces.sh
+++
@@ -1,4 +0,0 @@
-#Must be executed in a node with network connection, such as hpc-transfer.usc.edu
-
-wget
-wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/gencode.v27.annotation.gtf.gz
---
+++ download_references.sh
@@ -0,0 +1,13 @@
+#Must be executed in a node with network connection, such as hpc-transfer.usc.edu
+
+#Download human genome fasta (sequence) and .gtf (annotation) files from GENCODE. 
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/GRCh38.primary_assembly.genome.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/gencode.v27.annotation.gtf.gz
+
+#Downlaod rat genome fasta (sequence) and .gtf (annotation) files from ENSEMBL (identical to GenBank. GENCODE has no rat genome assemblies).
+wget ftp://ftp.ensembl.org/pub/release-91/fasta/rattus_norvegicus/dna/
+wget ftp://ftp.ensembl.org/pub/release-91/gtf/rattus_norvegicus
+
+#Downlaod mouse genome fasta (sequence) and .gtf (annotation) files from GENCODE.
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M16/GRCm38.primary_assembly.genome.fa.gz
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M16/gencode.vM16.annotation.gtf.gz
