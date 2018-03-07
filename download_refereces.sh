f7cdea49231d235ec6ad0ee6b0505f91f8f6bbf9
David Manahan <dmanahan@usc.edu>
2018-03-07 12:41:34 -0800

    Create download_refereces.sh

---
+++ download_refereces.sh
@@ -0,0 +1,4 @@
+#Must be executed in a node with network connection, such as hpc-transfer.usc.edu
+
+wget
+wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/gencode.v27.annotation.gtf.gz
