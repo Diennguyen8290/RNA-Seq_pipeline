4d64b64ce25b3e883b1bb32c3f8bc6a0383f07ab
David Manahan <dmanahan@usc.edu>
2018-03-27 14:50:50 -0700

    Update download_references.sh

--- download_references.sh
+++ download_references.sh
@@ -1,5 +1,8 @@
 #Must be executed in a node with network connection, such as hpc-transfer.usc.edu
 
+mkdir genome_references
+cd genome_references
+
 #Download human genome fasta (sequence) and .gtf (annotation) files from GENCODE. 
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/GRCh38.primary_assembly.genome.fa.gz
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/gencode.v27.annotation.gtf.gz
@@ -11,10 +14,6 @@ wget ftp://ftp.ensembl.org/pub/release-91/gtf/rattus_norvegicus/Rattus_norvegicu
 #Downlaod mouse genome fasta (sequence) and .gtf (annotation) files from GENCODE.
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M16/GRCm38.primary_assembly.genome.fa.gz
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M16/gencode.vM16.annotation.gtf.gz
-
-mkdir genome_references
-mv -t genome_references GRCh38.primary_assembly.genome.fa.gz gencode.v27.annotation.gtf.gz GCF_000001895.5_Rnor_6.0_genomic.fna.gz Rattus_norvegicus.Rnor_6.0.91.chr.gtf.gz GRCm38.primary_assembly.genome.fa.gz gencode.vM16.annotation.gtf.gz
-cd genome_references
 
 #Unzip all compressed fasta and .gtf files.
 gunzip -vf *gz
