8ef3502c0108bc8f76de2a60f4e5245ea3cb4ecd
David Manahan <dmanahan@usc.edu>
2018-03-30 18:49:09 -0700

    Update and rename fastqc_SE.pbs to fastqc_SE_slurm.sh

---
+++ pbs_scripts/FastQC/fastqc_SE_slurm.sh
@@ -0,0 +1,12 @@
+#SBATCH --job-name='fastqc_SE'
+#SBATCH --error=${IN}_fastqc_SE.error
+#SBATCH --output=${IN}_fastqc_SE.out
+#SBATCH -ntasks=1
+#SBATCH --mem=10000MB
+#SBATCH --mem-per-cpu=10000MB
+#SBATCH --time=10:00:00
+#SBATCH --partition=cmb
+#!/bin/bash
+cd "$SLURM_SUBMIT_DIR"
+
+fastqc ../FASTQ-DUMP/${IN}.fastq
