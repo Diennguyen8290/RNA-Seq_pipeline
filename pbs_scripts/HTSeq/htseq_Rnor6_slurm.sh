84f796943805228214f87868e905aec49445fea7
David Manahan <dmanahan@usc.edu>
2018-03-30 18:53:23 -0700

    Update and rename htseq_Rnor6.pbs to htseq_Rnor6_slurm.sh

---
+++ pbs_scripts/HTSeq/htseq_Rnor6_slurm.sh
@@ -0,0 +1,12 @@
+#SBATCH --job-name='count'
+#SBATCH --error=${IN}_count.error
+#SBATCH --output=${IN}_count.out
+#SBATCH --ntasks=1
+#SBATCH --mem=50000MB
+#SBATCH --mem-per-cpu=50000MB
+#SBATCH --time=10:00:00
+#SBATCH --partition=cmb
+#!/bin/bash
+cd "$SLURM_SUBMIT_DIR"
+
+samtools view ../STAR/${IN}Aligned.sortedByCoord.out.bam | htseq-count - ../../genome_references/Rattus_norvegicus.Rnor_6.0.91.chr.gtf --stranded=no > count_${IN}.txt
