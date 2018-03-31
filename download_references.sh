765e7565df5a4bb8877f7132a9d7e1148d8eebac
David Manahan <dmanahan@usc.edu>
2018-03-31 13:41:28 -0700

    Update download_references.sh

--- download_references.sh
+++ download_references.sh
@@ -7,8 +7,8 @@ cd genome_references
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/GRCh38.primary_assembly.genome.fa.gz
 wget ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_27/gencode.v27.annotation.gtf.gz
 
-#Download rat genome fasta (sequence) and .gtf (annotation) files from NCBI/ENSEMBL (Both databases are identical to each other. GENCODE has no rat genome assemblies).
-wget ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/001/895/GCF_000001895.5_Rnor_6.0/GCF_000001895.5_Rnor_6.0_genomic.fna.gz
+#Download rat genome fasta (sequence) and .gtf (annotation) files from NCBI/ENSEMBL (Both databases are identical to each other)
+wget ftp://ftp.ensembl.org/pub/release-91/fasta/rattus_norvegicus/dna/Rattus_norvegicus.Rnor_6.0.dna.toplevel.fa.gz
 wget ftp://ftp.ensembl.org/pub/release-91/gtf/rattus_norvegicus/Rattus_norvegicus.Rnor_6.0.91.chr.gtf.gz
 
 #Downlaod mouse genome fasta (sequence) and .gtf (annotation) files from GENCODE.
