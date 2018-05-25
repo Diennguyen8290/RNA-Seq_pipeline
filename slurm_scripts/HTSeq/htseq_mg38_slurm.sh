ffdc7371683b27dd71c27028670fb53200cea0c8
David Manahan <dmanahan@usc.edu>
2018-05-25 14:18:55 -0700

    Update htseq_mg38_slurm.sh

--- slurm_scripts/HTSeq/htseq_mg38_slurm.sh
+++ slurm_scripts/HTSeq/htseq_mg38_slurm.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 #SBATCH --job-name='count'
-#SBATCH --error=${IN}_count.error
-#SBATCH --output=${IN}_count.out
+#SBATCH --error=count.error
+#SBATCH --output=count.out
 #SBATCH --ntasks=1
 #SBATCH --mem=50000MB
 #SBATCH --mem-per-cpu=50000MB
 #SBATCH --time=10:00:00
 #SBATCH --partition=cmb
-#!/bin/bash
 cd "$SLURM_SUBMIT_DIR"
 
 samtools view ../STAR/${IN}Aligned.sortedByCoord.out.bam | htseq-count - ../../genome_references/gencode.vM16.annotation.gtf --stranded=no > count_${IN}.txt
+echo 'htseq-count's run has completed' > check_${IN}.txt
