5e73dee6ccb678ed5b510140ce41d25e8c237224
David Manahan <dmanahan@usc.edu>
2018-05-25 14:33:26 -0700

    Update trimgalore_PE_slurm.sh

--- slurm_scripts/TrimGalore/trimgalore_PE_slurm.sh
+++ slurm_scripts/TrimGalore/trimgalore_PE_slurm.sh
@@ -1,12 +1,9 @@
-#SBATCH --job-name='TrimGalore_PE'
-#SBATCH --error=${IN}_TrimGalore_PE.error
-#SBATCH --output=${IN}_TrimGalore_PE.out
+#!/bin/bash
 #SBATCH --ntasks=14
 #SBATCH --mem=10000MB
 #SBATCH --mem-per-cpu=10000MB
 #SBATCH --time=10:00:00
 #SBATCH --partition=cmb
-#!/bin/bash
 cd "$SLURM_SUBMIT_DIR"
 
 trim_galore --fastqc --paired ../FASTQ-DUMP/${IN}_1.fastq ../FASTQ-DUMP/${IN}_2.fastq
