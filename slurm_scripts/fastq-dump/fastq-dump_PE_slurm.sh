22f7197b2b42fef96aa9b0f2782c7fc9d3587fd4
David Manahan <dmanahan@usc.edu>
2018-05-25 14:12:01 -0700

    Update fastq-dump_PE_slurm.sh

--- slurm_scripts/fastq-dump/fastq-dump_PE_slurm.sh
+++ slurm_scripts/fastq-dump/fastq-dump_PE_slurm.sh
@@ -1,12 +1,10 @@
-#SBATCH --job-name='fastq-dump_PE'
-#SBATCH --error=${IN}_fastq-dump_PE.error
-#SBATCH --output=${IN}_fastq-dump_PE.out
+#!/bin/bash
 #SBATCH --ntasks=14
 #SBATCH --mem=10000MB
 #SBATCH --mem-per-cpu=10000MB
 #SBATCH --time=10:00:00
 #SBATCH --partition=cmb
-#!/bin/bash
 cd "$SLURM_SUBMIT_DIR"
 
 fastq-dump --split-files -I ../${IN}.sra
+echo 'fastq-dump has completed' > check_${IN}.txt
