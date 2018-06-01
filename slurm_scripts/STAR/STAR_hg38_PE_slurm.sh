dcc3057874eacec987c3089a422b8f38ab7ba901
David Manahan <dmanahan@usc.edu>
2018-05-31 17:35:07 -0700

    Update STAR_hg38_PE_slurm.sh

--- slurm_scripts/STAR/STAR_hg38_PE_slurm.sh
+++ slurm_scripts/STAR/STAR_hg38_PE_slurm.sh
@@ -1,12 +1,9 @@
-#SBATCH --job-name='STAR_hg38_SE'
-#SBATCH --error=${IN}_STAR_hg38_SE.error
-#SBATCH --output=${IN}_STAR_hg38_SE.out
+#!/bin/bash
 #SBATCH --ntasks=14
 #SBATCH --mem=50000MB
 #SBATCH --mem-per-cpu=50000MB
 #SBATCH --time=10:00:00
 #SBATCH --partition=cmb
-#!/bin/bash
 cd "$SLURM_SUBMIT_DIR"
 
 ref_genome=../../STAR_hg38_index
