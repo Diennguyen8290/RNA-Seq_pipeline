12a0de078a35b8d455aa329bc4c66eec785419db
David Manahan <dmanahan@usc.edu>
2018-06-04 10:56:41 -0700

    Update fastqc_PE_slurm.sh

--- slurm_scripts/FastQC/fastqc_PE_slurm.sh
+++ slurm_scripts/FastQC/fastqc_PE_slurm.sh
@@ -1,12 +1,9 @@
-#SBATCH --job-name='fastqc_PE'
-#SBATCH --error=${IN}_fastqc_PE.error
-#SBATCH --output=${IN}_fastqc_PE.out
+#!/bin/bash
 #SBATCH --ntasks=1
 #SBATCH --mem=10000MB
 #SBATCH --mem-per-cpu=10000MB
 #SBATCH --time=10:00:00
 #SBATCH --partition=cmb
-#!/bin/bash
 cd "$SLURM_SUBMIT_DIR"
 
 fastqc ../FASTQ-DUMP/${IN}_1.fastq
