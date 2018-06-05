0e6a876e7c26fe77e98bb9ba70084e64bf1a5f6f
David Manahan <dmanahan@usc.edu>
2018-06-04 18:37:46 -0700

    Update build_STAR_indices.sh

--- build_STAR_indices.sh
+++ build_STAR_indices.sh
@@ -1,3 +1,3 @@
-qsub pbs_scripts/STAR_index/STAR_hg38_index.pbs
-qsub pbs_scripts/STAR_index/STAR_Rnor6_index.pbs
-qsub pbs_scripts/STAR_index/STAR_mg38_index.pbs
+sbatch --job-name=build_hg38 --output=STARindex_hg38.out --error=STARindex_hg38.error slurm_scripts/STAR_index/STAR_hg38_index.slurm
+sbatch --job-name=build_Rnor6 --output=STARindex_Rnor6.out --error=STARindex_Rnor6.error slurm_scripts/STAR_index/STAR_Rnor6_index.slurm
+sbatch --job-name=build_mg38 --output=STARindex_mg38.out --error=STARindex_mg38.error slurm_scripts/STAR_index/STAR_mg38_index.slurm
