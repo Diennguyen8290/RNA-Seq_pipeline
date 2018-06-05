f220fb18bd751bfbdec2efb47532a85be8b8f8af
David Manahan <dmanahan@usc.edu>
2018-06-04 18:39:52 -0700

    Update STAR_hg38_index_slurm.sh

--- slurm_scripts/STAR_index/STAR_hg38_index_slurm.sh
+++ slurm_scripts/STAR_index/STAR_hg38_index_slurm.sh
@@ -1,21 +1,18 @@
-#SBATCH --job-name='STARindex_hg38'
-#SBATCH --error=STARindex_hg38.error
-#SBATCH --output=STARindex_hg38.out
+#!/bin/bash
 #SBATCH --ntasks=14
 #SBATCH --mem=50000MB
 #SBATCH --mem-per-cpu=50000MB
 #SBATCH --time=10:00:00
 #SBATCH --partition=cmb
-#!/bin/bash
 cd "$SLURM_SUBMIT_DIR"
 
 mkdir STAR_hg38_index
 cd STAR_hg38_index
 
-STAR --runThreadN 14 
+STAR --runThreadN 14 \
      --runMode genomeGenerate \
-     --genomeFastaFiles ../GRCh38.primary_assembly.genome.fa \
+     --genomeFastaFiles ../genome_references/GRCh38.primary_assembly.genome.fa \
      --genomeDir $PWD \
-     --sjdbGTFfile ../gencode.v27.annotation.gtf \
+     --sjdbGTFfile ../genome_references/gencode.v27.annotation.gtf \
 
 cd ..
